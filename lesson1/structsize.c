62b5499b36f64c9d164d3482820f05c3c901ec67
James Salter <jsalter@gmail.com>
2011-08-22 23:32:46 -0700

    Fix gcc warnings.

--- lesson1/structsize.c
+++ lesson1/structsize.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 
-main() {
+int main() {
     struct Things {
         char    aChar;
         int     intThingy;
         float   floatThingy;
         char *  ptrToChar;
         char    bChar;
-    } things;
+    };
+
     struct ThingsANew {
         char *  ptrToChar;
         int     intThingy;
         float   floatThingy;
         char    aChar;
         char    bChar;
-    } thingsANew;
+    };
 
-   
+    /* Why are these two struct's with the same data a different size? */
+    /* Could you tell by looking at them which is smaller? */
 
-    // Why are these two struct's with the same data a different size?
-    // Could you tell by looking at them which is smaller?
+    printf ("sizeof(Things)=%lu\n", sizeof(struct Things));
+    printf ("sizeof(ThingsANew)=%lu\n", sizeof(struct ThingsANew));
 
-    printf ("sizeof(Things)=%d\n", sizeof(struct Things));
-    printf ("sizeof(ThingsANew)=%d\n", sizeof(struct ThingsANew));
-
-   
+    return 0; 
 }
